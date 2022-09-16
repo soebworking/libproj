b99ab973772daf9c2c090d192cacfb06718ea092
Mohamedsoeb Ahmednashirwala <soeb.working@gmail.com>
2022-09-16 09:50:37 +0530

    Added JPA user repository

---
+++ src/main/java/com/library/library/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.library.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.library.library.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	User findByEmail(String email);
+}
