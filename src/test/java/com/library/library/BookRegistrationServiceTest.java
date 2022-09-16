ac193053ee9ad8707116986b521d1d89c3dfba66
Mohamedsoeb Ahmednashirwala <soeb.working@gmail.com>
2022-09-16 10:09:20 +0530

    added junit test

---
+++ src/test/java/com/library/library/BookRegistrationServiceTest.java
@@ -0,0 +1,35 @@
+package com.library.library;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import net.javaguides.springboot.model.Book;
+import net.javaguides.springboot.service.BookRegistrationService;
+
+@SpringBootTest
+public class BookRegistrationServiceTest {
+
+	 /**
+     * Autowire in the service we want to test
+     */
+    @Autowired
+	BookRegistrationService bookRegService;
+	
+	
+	@Test
+	public void findAllTest() {
+		
+		// Execute the service call
+        List<Book> book = bookRegService.findAll();
+        assertThat(book).isNotNull();
+		
+		
+	}
+	
+	
+}
